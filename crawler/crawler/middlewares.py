24fbf6299c07f7e0bc40ce758fbe69b24c431652
kshehjaar01 <kshehjaar.official@gmail.com>
2025-11-27 21:05:06 +0530

    Update middlewares.py

--- crawler/crawler/middlewares.py
+++ crawler/crawler/middlewares.py
@@ -1,16 +1,30 @@
-# middlewares for the crawler project.
-# You can enable and customize middlewares here if needed.
-
+import logging
 from scrapy import signals
+from scrapy.downloadermiddlewares.retry import RetryMiddleware
+from scrapy.utils.response import response_status_message
 
-class CrawlerSpiderMiddleware:
+logger = logging.getLogger(__name__)
+
+class ErrorLoggingMiddleware(RetryMiddleware):
+    """
+    Extends Scrapy's RetryMiddleware to log errors to a file via spider.logger
+    """
+    def __init__(self, settings):
+        super().__init__(settings)
+        self.max_retry_times = settings.getint('RETRY_TIMES', 3)
+
     @classmethod
     def from_crawler(cls, crawler):
-        # This method is used by Scrapy to create your spiders.
-        s = cls()
-        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
-        return s
+        return cls(crawler.settings)
 
-    def spider_opened(self, spider):
-        spider.logger.info("Spider opened: %s", spider.name)
+    def process_response(self, request, response, spider):
+        # If response status is 4xx/5xx, log it
+        if response.status >= 400:
+            spider.logger.warning("HTTP %s on %s", response.status, response.url)
+        return super().process_response(request, response, spider)
+
+    def process_exception(self, request, exception, spider):
+        spider.logger.error("Exception for %s: %s", request.url, exception)
+        return super().process_exception(request, exception, spider)
+
 
