683485a194be6642b0bc57f35928b98ffa079d3b
kshehjaar01 <kshehjaar.official@gmail.com>
2025-11-27 17:17:02 +0530

    Update middlewares.py

--- crawler/crawler/middlewares.py
+++ crawler/crawler/middlewares.py
@@ -1,1 +1,16 @@
+# middlewares for the crawler project.
+# You can enable and customize middlewares here if needed.
+
+from scrapy import signals
+
+class CrawlerSpiderMiddleware:
+    @classmethod
+    def from_crawler(cls, crawler):
+        # This method is used by Scrapy to create your spiders.
+        s = cls()
+        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+        return s
+
+    def spider_opened(self, spider):
+        spider.logger.info("Spider opened: %s", spider.name)
 
